7207da99e384c4d0255c7b8942d178bb2513b4d1
Eduard Vaks <eddie.vaks@aviatnet.com>
2022-04-06 13:55:57 +1200

    add renew cert script for prod

---
+++ scripts/renew-cert.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+DIR=/var/www
+
+certbot renew --webroot --config-dir ${DIR}/letsencrypt/config --work-dir ${DIR}/letsencrypt/work --logs-dir ${DIR}/letsencrypt/log --webroot-path ${DIR}/resources/htdocs --cert-path ${DIR}/letsencrypt/certs --cert-path ${DIR}/letsencrypt/certs/cert.pem --key-path ${DIR}/letsencrypt/certs/priv.pem
+
+chown budget-app /letsencrypt/config/archive/budgetservice.org.nz/*/*
+
+chgrp budget-app /letsencrypt/config/archive/budgetservice.org.nz/*/*
