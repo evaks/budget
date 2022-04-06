3f8b182ca0fba19a84b27d7ab51c2b7c8d2bef12
Eduard Vaks <eddie.vaks@aviatnet.com>
2022-04-06 13:59:57 +1200

    add path to renew certs

--- scripts/renew-cert.sh
+++ scripts/renew-cert.sh
@@ -3,6 +3,6 @@ DIR=/var/www
 
 certbot renew --webroot --config-dir ${DIR}/letsencrypt/config --work-dir ${DIR}/letsencrypt/work --logs-dir ${DIR}/letsencrypt/log --webroot-path ${DIR}/resources/htdocs --cert-path ${DIR}/letsencrypt/certs --cert-path ${DIR}/letsencrypt/certs/cert.pem --key-path ${DIR}/letsencrypt/certs/priv.pem
 
-chown budget-app /letsencrypt/config/archive/budgetservice.org.nz/*/*
+chown budget-app ${DIR}/letsencrypt/config/archive/budgetservice.org.nz/*/*
 
-chgrp budget-app /letsencrypt/config/archive/budgetservice.org.nz/*/*
+chgrp budget-app ${DIR}/letsencrypt/config/archive/budgetservice.org.nz/*/*
