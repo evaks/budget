d925100337389fba017da0b54663e257ba2464f8
Eduard Vaks <eddie.vaks@aviatnet.com>
2023-10-06 09:04:59 +1300

    fix active client report

--- plugins/budget_reports/client_closed.sql
+++ plugins/budget_reports/client_closed.sql
@@ -1,6 +1,6 @@
 SELECT NULL id,
   firstname, lastname,
-  SUM(open) opened, SUM(closed) closed, sum(active) active, sum(used) used FROM (
+  SUM(open) opened, SUM(closed) closed, max(active) active, sum(used) used FROM (
   SELECT 
     new.firstname firstname, new.lastname,
     old.start is NULL OR DATE_ADD(old.start, INTERVAL 5 MONTH) < new.last open, 
@@ -12,8 +12,9 @@ SELECT NULL id,
   ON new.firstname = old.firstname and new.lastname = old.lastname
   UNION SELECT 
    old.firstname, old.lastname, 0 open, 
-   new.last is NULL OR DATE_ADD(old.start, INTERVAL 5 MONTH) < new.last closed,
-   0 active,
+   (new.last is NULL AND DATE_ADD(old.start, INTERVAL 5 MONTH) < from_unixtime(floor(?:stop:/1000)))
+    OR (new.last is NOT NULL AND DATE_ADD(old.start, INTERVAL 5 MONTH)  < new.last) closed,
+   DATE_ADD(old.recent, INTERVAL 5 MONTH)  >=  from_unixtime(floor(?:stop:/1000)) active,
    0 used
   FROM (@newclient) new
   RIGHT JOIN (@oldclient) old
